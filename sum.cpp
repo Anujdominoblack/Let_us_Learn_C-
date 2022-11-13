922bf31f2b98959a4ecd3fa16738044093059eaf
anuj <anujkumarpandey2727@gmail.com>
2022-11-14 01:23:29 +0530

    sum of two numbers

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>//header file
+using namespace std;
+int main() //code reading start from here
+{
+    int a,b,c; //three variables created
+    cin>>a>>b; //taking input from the user
+    c=a+b;  //performing calculation 
+    cout<<c; //return result
+    return 0;
+}
