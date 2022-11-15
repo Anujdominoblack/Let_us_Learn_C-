673ec1c4c6b08fe5be32feaebcd372cc4868e1e2
anuj <anujkumarpandey2727@gmail.com>
2022-11-15 18:31:36 +0530

    multiplication

---
+++ multiply.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;//declaring variables
+    cin>>a>>b;//taking input
+    c=a*b;//arithematic operation
+    cout<<c; //returning result
+    return 0; //since its not void we will return 0
+}
