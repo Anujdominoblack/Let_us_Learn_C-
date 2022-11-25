84ba571abde5f8deefc7cfafc307c5db84861dfc
anuj <anujkumarpandey2727@gmail.com>
2022-11-26 01:34:34 +0530

    divide

---
+++ divide.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;//variables declared
+    cin>>a; //value assigned
+    cin>>b;  //value assigned
+    int c=b/a;//arithematic operation 
+    cout<<c;//output is collected
+}
