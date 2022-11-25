8015bac81ae36d28385c963232f142a1e116ff5a
anuj <anujkumarpandey2727@gmail.com>
2022-11-26 02:33:10 +0530

    if else is added

---
+++ else.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a>0)
+    cout<<"a positiive number is entered";
+    else
+    cout<<"a negative  number is entered";
+    return 0;
+}
