045b8712871271dae345f089f54f620eccf5433f
anuj <anujkumarpandey2727@gmail.com>
2022-11-27 00:13:58 +0530

    for loop and while loop tut.cpp

---
+++ forloop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;//variables declared
+    cin>>a; //number is input through the keyboard
+    for(int i=0;i<=a;i++) // for loop is created
+    cout<<i<<endl;  //number is print out
+    return 0;
+
+}
---
+++ whileloop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int i=0;
+    while(i<=a){
+    cout<<i<<endl;
+    i++;}
+    return 0;
+}
