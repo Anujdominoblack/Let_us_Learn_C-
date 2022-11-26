29b5f5a06a7970452d35cf2a7063ab34fdfccccc
anuj <anujkumarpandey2727@gmail.com>
2022-11-26 21:19:22 +0530

    it is if else ladder .cpp

---
+++ ifelse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a>100)
+    cout<<"it is greater than 100";
+    else if(a>50)
+    cout<<"it is greater than 50";
+    else
+    cout<<"it is less than 50";
+    return 0;
+}
