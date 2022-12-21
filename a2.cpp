5b0af928ee2ce508b3a184212240fc5d3d4aa261
anuj <anujkumarpandey2727@gmail.com>
2022-12-21 21:31:45 +0530

    array intialization tut.cpp

---
+++ a2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={10,20}; //other members are initialized to zero
+    return 0;
+}
---
+++ a3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]{10,20,30,40,50}; //initializer list initialization 
+    int arr2[]{10,20,23};
+    int arr3[10]{1,2};
+    return 0;
+}
