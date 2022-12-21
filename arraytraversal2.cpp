918734a8d38d0dd1dd9250b038a977709a9ede3b
anuj <anujkumarpandey2727@gmail.com>
2022-12-21 23:58:07 +0530

    another method of array traversal tut.cpp

---
+++ arraytraversal2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int arr[]={10,20,30,40,45}; //array created
+  for(int x:arr)  //loop created
+   cout<<x<<" ";  //displaying the values
+  return 0; //code ends
+}
