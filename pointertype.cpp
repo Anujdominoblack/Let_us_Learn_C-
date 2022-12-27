53cce1c563d88603c7af95a5d1d44f2e27ba6dc2
anuj <anujkumarpandey2727@gmail.com>
2022-12-27 22:46:58 +0530

    type of pointers in c++ tut.cpp

---
+++ pointertype.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *p1; //pointer of type  int
+    double *p2;  //pointer of type double
+    string *p3;  //pointer of type string 
+    cout<<sizeof(p1)<<" ";   //displaying the size of p1
+    cout<<sizeof(p2)<<" ";  //displaying the size of p2
+    cout<<sizeof(p3)<<" ";  //displaying the size of p3
+    return 0;
+}
