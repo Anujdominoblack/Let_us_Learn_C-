c4e2b6388bfa10ca06be2dc9cb6ea6d5f60f159c
anuj <anujkumarpandey2727@gmail.com>
2023-02-13 22:59:53 +0530

    lembda capture list by value

---
+++ cpaturelistnoreference.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=5,y=20;
+    auto lembda=[=](int a) mutable{x=x+a;
+                           y=y+a;};
+    lembda(20);
+    cout<<x<" "<<y;
+    return 0;
+}
