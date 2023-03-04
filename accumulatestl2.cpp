4217adc42aa4fec07a979122f568a5f61f009782
anuj <anujkumarpandey2727@gmail.com>
2023-03-04 15:29:01 +0530

    accumulate minus

---
+++ accumulatestl2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<numeric>
+using namespace std;
+int main()
+{
+    vector<int> v={10,20,30};
+    int res=100;
+    cout<<accumulate(v.begin(),v.end(),res,minus<int>); //it subtracts element 
+    return 0;
+}
