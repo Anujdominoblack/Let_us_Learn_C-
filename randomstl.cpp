4d66d219c61fe0f5d247374351927e46a781ab08
anuj <anujkumarpandey2727@gmail.com>
2023-03-04 15:38:28 +0530

    random number generator

---
+++ randomstl.cpp
@@ -0,0 +1,12 @@
+//to generate random numbers
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        cout<<rand()<<endl;  //it generates random function
+    }
+    return 0;
+}
