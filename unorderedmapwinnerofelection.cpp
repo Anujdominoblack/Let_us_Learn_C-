d315a044bc4d78025c600fc2cb71d9ae471bb2a5
anuj <anujkumarpandey2727@gmail.com>
2023-03-21 22:41:54 +0530

    unorderd map problem 2  election winner

---
+++ unorderedmapwinnerofelection.cpp
@@ -0,0 +1,29 @@
+//winner of election
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+string findwinner(string arr[],int n)
+{
+    unordered_map <string,int> m;
+    for(int i=0;i<n;i++)
+    {
+        m[arr[i]]++;
+    }
+    int max=0;string winner;
+    for(auto x:m)
+    {
+        if(x.second>max)
+        {
+            max=x.second;
+            winner=x.first;
+        }
+    }
+    return winner;
+}
+int main()
+{
+    string arr[]={"abc","xyz","abc","pqr"};
+    int n =sizeof(arr)/sizeof(arr[0]);
+    cout<<findwinner(arr,n);
+    return 0;
+}
