ce17c01408fab678e83ba7feac14a6c631eec921
anuj <anujkumarpandey2727@gmail.com>
2023-03-30 00:06:11 +0530

    USE OF str

---
+++ useofstr.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;  //string is declared
+    cout<<"enter your name";
+    cin>>name;   //input is taken
+    cout<<"your name is "<<name;  //output
+}
